8760efdc1146b92d149f577a6b4ecd4f26ab9ba4
Azulay Amit 2 <aa85378@elta.co.il>
2019-12-25 18:35:49 +0200

    rem index test2

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,29 +1,29 @@
-var expect = require("chai").expect;
-var request = require("request");
+// var expect = require("chai").expect;
+// var request = require("request");
 
-describe("index.js", function() {
-  context("test REST", () => {
-    it("Main page content", function(done) {
-      request("http://localhost:8080", function(error, response, body) {
-        if (!error) {
-          expect(body).to.equal("Hello World");
-          done();
-        }
-      });
-    });
+// describe("index.js", function() {
+//   context("test REST", () => {
+//     it("Main page content", function(done) {
+//       request("http://localhost:8080", function(error, response, body) {
+//         if (!error) {
+//           expect(body).to.equal("Hello World");
+//           done();
+//         }
+//       });
+//     });
 
-    it("Main page status", function(done) {
-      request("http://localhost:8080", function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
+//     it("Main page status", function(done) {
+//       request("http://localhost:8080", function(error, response, body) {
+//         expect(response.statusCode).to.equal(200);
+//         done();
+//       });
+//     });
 
-    it("About page content", function(done) {
-      request("http://localhost:8080/about", function(error, response, body) {
-        expect(response.statusCode).to.equal(404);
-        done();
-      });
-    });
-  });
-});
+//     it("About page content", function(done) {
+//       request("http://localhost:8080/about", function(error, response, body) {
+//         expect(response.statusCode).to.equal(404);
+//         done();
+//       });
+//     });
+//   });
+// });
